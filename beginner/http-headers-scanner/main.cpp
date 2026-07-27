1b143a5f0cd8751d2c662015100108b6344f514b
DrakkuN <dracnguyenbku@gmail.com>
2026-07-27 06:13:31 -0400

    init

---
+++ beginner/http-headers-scanner/main.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+ifstream inFile("http-check.txt");
+ofstream outFile("result.txt");
+
+int main(){
+    string line;
+    
+    if (inFile.is_open()) {
+        while (getline(inFile, line)) {
+            if (outFile.is_open()) {
+                outFile << line << "\n";
+                outFile << "Result: ";
+            } else {
+                cerr << "Error opening file for writing!\n";
+            }
+            
+            outFile << "----------------" << endl;
+        }
+        inFile.close(); // Close the file stream
+
+        outFile.close(); // Close the file stream
+    } else {
+        cerr << "Error opening file for reading!\n";
+    }
+    cout << "HTTP Scanneris running... Don't shutdown or exit!" << endl;
+}
